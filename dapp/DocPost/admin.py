ec131061aeade131eda0ece6ca6d53e92d394b6f
sudarshanmestha <sudarshan15399@gmail.com>
2025-11-08 20:37:23 +0530

    updated doc and setting

--- dapp/DocPost/admin.py
+++ dapp/DocPost/admin.py
@@ -1,12 +1,11 @@
+# DocPost/admin.py
 from django.contrib import admin
 from .models import DocPosts
-# Register your models here.
-
 
 @admin.register(DocPosts)
-class DocPostAdmin(admin.ModelAdmin):
+class DocPostsAdmin(admin.ModelAdmin):
     list_display = ('title', 'date', 'is_published')
     list_filter = ('is_published', 'date')
-    search_fields = ('title', 'content')
+    search_fields = ('title', 'description')
     prepopulated_fields = {'slug': ('title',)}
     readonly_fields = ('date',)
